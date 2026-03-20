812d61b856fcc330d7b6c91b556df0a3104c1209
Tom Smith <ThomUK@users.noreply.github.com>
2026-03-20 22:49:26 +0000

    button location move

--- app.R
+++ app.R
@@ -10,46 +10,48 @@ ui <- fluidPage(
     div(
       class = "header-row",
       tags$h1("Sketch"),
+      tags$button("Clear", onclick = "clearCanvas()"),
+      tags$button("Export", class = "primary", onclick = "openInNewWindow()"),
       div(
         class = "toolbar",
-      span(class = "toolbar-label", "Colour"),
-      tags$div(
-        class = "toolbar-circle colour-swatch active", style = "background:#222222;",
-        title = "Black", onclick = "setColour('#222222', this)"
-      ),
-      tags$div(
-        class = "toolbar-circle colour-swatch", style = "background:#C0C0C0;",
-        title = "Silver", onclick = "setColour('#C0C0C0', this)"
-      ),
-      tags$div(
-        class = "toolbar-circle colour-swatch", style = "background:#0F52BA;",
-        title = "Blue", onclick = "setColour('#0F52BA', this)"
-      ),
-      tags$div(
-        class = "toolbar-circle colour-swatch", style = "background:#FF3131;",
-        title = "Red", onclick = "setColour('#FF3131', this)"
-      ),
-      tags$div(
-        class = "toolbar-circle colour-swatch", style = "background:#FFA500;",
-        title = "Orange", onclick = "setColour('#FFA500', this)"
-      ),
-      tags$div(
-        class = "toolbar-circle colour-swatch", style = "background:#32CD32;",
-        title = "Green", onclick = "setColour('#32CD32', this)"
-      ),
-      div(class = "divider"),
-      span(class = "toolbar-label", "Size"),
-      tags$input(
-        type = "range", min = "1", max = "24", value = "4",
-        oninput = "setSize(this.value)"
-      ),
-      div(class = "divider"),
-      span(class = "toolbar-label", "Erase"),
-      tags$div(
-        id = "eraserToggle", class = "toolbar-circle eraser-toggle",
-        title = "Erase strokes of the current colour",
-        onclick = "toggleEraser()"
-      )
+        span(class = "toolbar-label", "Colour"),
+        tags$div(
+          class = "toolbar-circle colour-swatch active", style = "background:#222222;",
+          title = "Black", onclick = "setColour('#222222', this)"
+        ),
+        tags$div(
+          class = "toolbar-circle colour-swatch", style = "background:#C0C0C0;",
+          title = "Silver", onclick = "setColour('#C0C0C0', this)"
+        ),
+        tags$div(
+          class = "toolbar-circle colour-swatch", style = "background:#0F52BA;",
+          title = "Blue", onclick = "setColour('#0F52BA', this)"
+        ),
+        tags$div(
+          class = "toolbar-circle colour-swatch", style = "background:#FF3131;",
+          title = "Red", onclick = "setColour('#FF3131', this)"
+        ),
+        tags$div(
+          class = "toolbar-circle colour-swatch", style = "background:#FFA500;",
+          title = "Orange", onclick = "setColour('#FFA500', this)"
+        ),
+        tags$div(
+          class = "toolbar-circle colour-swatch", style = "background:#32CD32;",
+          title = "Green", onclick = "setColour('#32CD32', this)"
+        ),
+        div(class = "divider"),
+        span(class = "toolbar-label", "Size"),
+        tags$input(
+          type = "range", min = "1", max = "24", value = "4",
+          oninput = "setSize(this.value)"
+        ),
+        div(class = "divider"),
+        span(class = "toolbar-label", "Erase"),
+        tags$div(
+          id = "eraserToggle", class = "toolbar-circle eraser-toggle",
+          title = "Erase strokes of the current colour",
+          onclick = "toggleEraser()"
+        )
       )
     ),
     div(
@@ -64,11 +66,6 @@ ui <- fluidPage(
         id = "imageCaption", type = "text",
         placeholder = "Add an optional caption to your sketch...",
       )
-    ),
-    div(
-      class = "btn-row",
-      tags$button("Clear", onclick = "clearCanvas()"),
-      tags$button("Export", class = "primary", onclick = "openInNewWindow()")
     ),
     tags$div(id = "copyStatus", "")
   )
